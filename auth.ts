61743494106d7416406c1b76319ac1de93983fa3
Koki <kokicodes@gmail.com>
2024-02-25 08:10:00 -0500

    auth

--- auth.ts
+++ auth.ts
@@ -1,11 +1,9 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 
-const prisma = new PrismaClient();
 
 export const {
   handlers: { GET, POST },
@@ -13,11 +11,29 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    async signIn({ user }) {
+      if (user) {
+        return true;
+      }
+      return false;
+    },
+    async jwt({ token, account, profile }) {
+      console.log(token, "token", account, "account",profile);
+      return token;
+    },
+  },
   pages: {
     signIn: "/login",
     error: "/error",
   },
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   secret: process.env.NEXTAUTH_SECRET,
   ...authConfig,
