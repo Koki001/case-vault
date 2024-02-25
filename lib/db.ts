3ee65c2b6baac39215f1405194c8c04b0c9a769b
Koki <kokicodes@gmail.com>
2024-02-25 08:48:09 -0500

    prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
+// Declare globalThis with an index signature to avoid TypeScript error
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+let prisma: PrismaClient | undefined;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!globalThis.prisma) {
+    globalThis.prisma = new PrismaClient();
+  }
+  prisma = globalThis.prisma;
+}
+
+export const db = prisma;
